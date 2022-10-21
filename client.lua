23652a88ae98fd625f6e8f1d980a9f63d7118431
MonsieurPropre <111458186+MonsieurPropre@users.noreply.github.com>
2022-10-21 10:10:47 +0200

    Update client.lua

--- client.lua
+++ client.lua
@@ -44,17 +44,6 @@ AddEventHandler("MP:tok", function(coords)
     end
 end)
 
-RegisterNetEvent("MP:tak")
-AddEventHandler("MP:tak", function()
-    Citizen.Wait(100)
-    if a then
-        ESX.Game.Teleport(a, GetEntityCoords(PlayerPedId()))
-        a = false
-    else
-        ESX.ShowNotification("Mdrrr ça cheat ?")
-    end
-end)
-
 Citizen.CreateThread(function()
     for i = 1, #Config.tp, 1 do
         RegisterCommand(Config.tp[i].nameCommand, function(source, args)
@@ -75,51 +64,5 @@ Citizen.CreateThread(function()
                 end
             end
         end)
-    end
-
-
-    for i = 1, #Config.tp2, 1 do
-        RegisterCommand(Config.tp2[i].nameCommand, function(source, args)
-            local check = checkperm(Config.tp2[i].perm)
-            if check then
-                if Config.tp2[i].b == "tp" then
-                    if args[1] then
-                        local coords = GetEntityCoords(PlayerPedId())
-                        ESX.TriggerServerCallback("MP:tp", function(cb)
-                            if cb then
-                                ESX.ShowNotification("Vous avez bien téléporté le joueur !")
-                                a = true
-                            else
-                                ESX.ShowNotification("L'id n'es pas valide'!")
-                            end
-                        end, args[1], coords)
-                    end
-                else
-                    if args[1] then
-                        ESX.TriggerServerCallback("MP:tpa", function(cb)
-                            if cb then
-                                a = PlayerPedId()
-                                ESX.ShowNotification("Vous vous êtes bien téléporté sur le joueur !")
-                            else
-                                ESX.ShowNotification("L'id n'es pas valide'!")
-                            end
-                        end, args[1])
-                    end
-                end
-            end
-        end)
-    end
-end)
-
-Citizen.CreateThread(function()
-    while true do
-        for i = 1, #Config.pos do
-            if #(Config.pos[i].coords-GetEntityCoords(PlayerPedId())) < 2 then
-                if IsControlJustPressed(0, 51) then
-                    show(false, Config.pos[i].num)
-                end
-            end
-        end
-        Citizen.Wait(1)
     end
 end)
